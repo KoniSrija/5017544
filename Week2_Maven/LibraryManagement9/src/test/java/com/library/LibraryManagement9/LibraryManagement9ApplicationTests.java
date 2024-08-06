791e6324d4d84ed729fe158541aa74bf23a2c218
KoniSrija <konisreeja@gmail.com>
2024-08-06 22:43:54 +0530

    Add files via upload

---
+++ Week2_Maven/LibraryManagement9/src/test/java/com/library/LibraryManagement9/LibraryManagement9ApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.library.LibraryManagement9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
